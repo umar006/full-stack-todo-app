a209b069ebe02fead3424bb4b48fb28325800fcc
umar006 <umaralfaruq597@gmail.com>
2024-04-11 20:42:48 +0700

    feat: get todos from dummyjson todos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
 function App() {
-  return "Hello,todoapp";
+  const { data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await fetch("https://dummyjson.com/todos");
+      return res.json();
+    },
+  });
+  console.log(data);
+
+  return <h1>TODO</h1>;
 }
 
 export default App;
