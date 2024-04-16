257b8d596e883b40cad73d3ba6f72e6fa9879bad
umar006 <umaralfaruq597@gmail.com>
2024-04-16 09:42:47 +0700

    feat: setup postgre database

--- backend/go.mod
+++ backend/go.mod
@@ -3,8 +3,13 @@ module todo.umaru.run
 go 1.19
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/labstack/echo/v4 v4.11.4 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/labstack/echo/v4 v4.11.4
+	github.com/lib/pq v1.10.9
+)
+
+require (
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
 type Todo struct {
@@ -14,6 +18,15 @@ type Todo struct {
 }
 
 func main() {
+	db, err := sqlx.Open("postgres", os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatalf("error opening database: %w", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("error connecting to database: %w", err)
+	}
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error { return c.String(http.StatusOK, "Hello, World!") })
