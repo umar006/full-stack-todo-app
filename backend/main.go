61e2217d169e471d03b8e99c568cf18869a9738a
umar006 <umaralfaruq597@gmail.com>
2024-04-17 17:07:56 +0700

    feat(logger): json format and save log to file in production

--- backend/main.go
+++ backend/main.go
@@ -39,6 +39,21 @@ func main() {
 		TimeFormat: time.RFC3339,
 	}
 
+	if os.Getenv("APP_ENV") != "development" {
+		if _, err := os.Stat("logs"); os.IsNotExist(err) {
+			err := os.Mkdir("logs", 0700)
+			if err != nil {
+				log.Fatalf("failed make directory: %v", err)
+			}
+		}
+
+		file, err := os.OpenFile("./logs/todo.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+		if err != nil {
+			log.Fatalf("error opening todo log: %v", err)
+		}
+		output = zerolog.MultiLevelWriter(os.Stderr, file)
+	}
+
 	logger := zerolog.New(output).With().Timestamp().Logger()
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
 		LogURI:       true,
