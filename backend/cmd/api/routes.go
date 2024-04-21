fa3007953258b0a94bd74e34baf783ceec874c7e
umar006 <umaralfaruq597@gmail.com>
2024-04-21 17:23:56 +0700

    feat: create api to signup

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+
+	"todo.umaru.run/internal/models"
 )
 
 func routes(e *echo.Echo, db *sqlx.DB) {
@@ -11,4 +16,19 @@ func routes(e *echo.Echo, db *sqlx.DB) {
 	todoRoutes.POST("", handleCreateTodo(db))
 	todoRoutes.PUT("/:todoId", handleUpdateTodo(db))
 	todoRoutes.DELETE("/:todoId", handleDeleteTodo(db))
+
+	authRoutes := e.Group("/api/auth")
+	authRoutes.POST("/signup", func(c echo.Context) error {
+		user := models.User{ID: uuid.New()}
+		if err := c.Bind(&user); err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+
+		_, err := db.NamedExec("INSERT INTO users (id, username, password) VALUES (:id, :username, :password)", user)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+
+		return c.JSON(http.StatusCreated, response{"user": user})
+	})
 }
