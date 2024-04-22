ca28a8331262347fc1de84d2f70de5c8f098138e
umar006 <umaralfaruq597@gmail.com>
2024-04-22 14:18:55 +0700

    feat: jwt middleware authorization

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/jmoiron/sqlx"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"todo.umaru.run/internal/auth"
 )
 
 func routes(e *echo.Echo, db *sqlx.DB) {
 	todoRoutes := e.Group("/api/todos")
+	todoRoutes.Use(echojwt.JWT(auth.JWT_SIGNATURE_KEY))
+
 	todoRoutes.GET("", handleGetTodos(db))
 	todoRoutes.POST("", handleCreateTodo(db))
 	todoRoutes.PUT("/:todoId", handleUpdateTodo(db))
