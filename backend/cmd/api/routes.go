9b261b7643b56412cbaf3d9d0179ec44aa943252
umar006 <umaralfaruq597@gmail.com>
2024-04-22 14:31:11 +0700

    fix: format response error in jwt middleware

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/jmoiron/sqlx"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+
 	"todo.umaru.run/internal/auth"
 )
 
 func routes(e *echo.Echo, db *sqlx.DB) {
 	todoRoutes := e.Group("/api/todos")
-	todoRoutes.Use(echojwt.JWT(auth.JWT_SIGNATURE_KEY))
+	todoRoutes.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey: auth.JWT_SIGNATURE_KEY,
+		ErrorHandler: func(c echo.Context, err error) error {
+			return c.JSON(http.StatusUnauthorized, response{"error": err.Error()})
+		},
+	}))
 
 	todoRoutes.GET("", handleGetTodos(db))
 	todoRoutes.POST("", handleCreateTodo(db))
