77610e1bc281cc92c039a4b048d4a65471134817
umar006 <umaralfaruq597@gmail.com>
2024-04-25 12:23:32 +0700

    feat(web): styling add todo form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,14 +43,21 @@ function App() {
       <Auth />
       <h1>TODO</h1>
       <form onSubmit={handleAddTodo}>
-        <input
-          className="placeholder:italic placeholder:text-slate-400 block bg-white w-1/4 border border-slate-300 rounded-md py-3 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-          placeholder="Add new todo..."
-          type="text"
-          value={todo}
-          onChange={handleTodoChange}
-        />
-        <button type="submit">add</button>
+        <div className="space-x-4 flex justify-center items-center">
+          <input
+            className="placeholder:italic placeholder:text-slate-400 bg-white w-1/4 border border-slate-300 rounded-md py-3 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="Add new todo..."
+            type="text"
+            value={todo}
+            onChange={handleTodoChange}
+          />
+          <button
+            type="submit"
+            className="font-bold text-center uppercase transition-all text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          >
+            add
+          </button>
+        </div>
       </form>
       <TodoList queryTodos={data} />
     </>
